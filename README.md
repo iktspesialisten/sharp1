ac177f883c92a6d50e9134a229a416b7fa62b6ce
Sigurd Skauvik <sigurdsk@iktss.onmicrosoft.com>
2020-03-26 01:16:15 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This is my first C# app in linux
+Worth a try even thou .net is a bitch to install on the system
---
+++ helloworld1.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace helloworld
+{
+	class helloworld
+	{
+		public static void Main (string[] args)
+
+		{
+	
+			Console.WriteLine("Hello World!")
+		}
+	}
+}
